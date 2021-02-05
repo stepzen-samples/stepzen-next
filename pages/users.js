2bc3bda4f6d0781506e375f86e35e75e543bbf86
ajcwebdev <ac@ac.local>
2021-02-05 02:10:22 -0800

    Use getStaticProps to fetch users from jsonplaceholder

--- pages/users.js
+++ pages/users.js
@@ -1,7 +1,21 @@
-const Users = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { users: data }
+  }
+}
+
+const Users = ({ users }) => {
   return (
     <div>
       <h1>Users</h1>
+      {users.map(user => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+        </div>
+      ))}
     </div>
   );
 }
