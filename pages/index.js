deb785636b4f11e75b5e2f09230587e1afdb0633
ajcwebdev <ac@ac.local>
2021-02-05 01:30:58 -0800

    Change footer to link to StepZen

--- pages/index.js
+++ pages/index.js
@@ -45,12 +45,11 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://stepzen.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" />
+          Powered by StepZen
         </a>
       </footer>
     </div>
